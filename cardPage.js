05f2bcaf4b2ef4ea36a8fc7c9868b67f518b2131
Sulbha-06 <sulbha06@gmail.com>
2023-01-21 02:36:57 +0530

    added functionality

--- cardPage.js
+++ cardPage.js
@@ -6,8 +6,13 @@ const card4 = [6,7];
 const map = new Map();
 
 const onShuffel = () =>{
+    document.getElementById('1').firstChild.innerHTML = 2;
+    document.getElementById('1').style.background = '#2B8EAD';
+    console.log(document.getElementById('1').innerText);
     console.log("shuffel is clicked--->");
 }
 const onSort = ()=>{
+    document.getElementById('1').firstChild.innerHTML = 1;
+    document.getElementById('1').style.background = '#6F98A8';
     console.log("sort the number");
 }
