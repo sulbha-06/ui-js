74049431acf6b70d1deca8e098c2551df0bc7d4b
Sulbha-06 <sulbha06@gmail.com>
2023-01-21 01:52:11 +0530

    shuffel numbers ui

---
+++ cardPage.js
@@ -0,0 +1,13 @@
+const card1 = [1,8];
+const card2 = [2,4];
+const card3 = [3,5,9];
+const card4 = [6,7];
+
+const map = new Map();
+
+const onShuffel = () =>{
+    console.log("shuffel is clicked--->");
+}
+const onSort = ()=>{
+    console.log("sort the number");
+}
